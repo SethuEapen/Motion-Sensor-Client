fbbd4184bfd5f45f3daf118154026822f2040f51
Sethu Eapen <sethueapen@gmail.com>
2019-08-01 15:27:02 -0700

    Motion Sensor Final + Archives

---
+++ Motion-Attempts/RPi_Keyboard_Example.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import RPi.GPIO as GPIO
+import time
+pir_sensor = 11
+#closed = False;
+NULL_CHAR = chr(0)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pir_sensor, GPIO.IN)
+
+def write_report(report):
+    with open('/dev/hidg0', 'rb+') as fd:
+        fd.write(report.encode())
+
+time.sleep(5)
+print("Initialization over. System armed")
+current_state = 0
+try:
+    while True:
+        time.sleep(0.1)
+        current_state = GPIO.input(pir_sensor)
+        if current_state == 1: #and closed == False:
+            print("GPIO pin %s is %s" % (pir_sensor, current_state))
+            write_report(chr(5)+NULL_CHAR+chr(26)+NULL_CHAR*5)
+            write_report(NULL_CHAR*8)
+            time.sleep(5)
+            #closed = True
+            #break
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
